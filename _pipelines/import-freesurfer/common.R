343511793000b3022750ccce76a932a15913cf07
dipterix <dipterix.wang@gmail.com>
2022-06-18 01:54:23 -0400

    Removed old pipelines

--- _pipelines/import-freesurfer/common.R
+++
@@ -1,5 +0,0 @@
-target_name <- "import-freesurfer"
-target_script <- "make-import-freesurfer.R"
-target_directory <- "shared"
-Sys.setenv("TAR_PROJECT" = target_name)
-Sys.setenv(TAR_WARN = "false")
